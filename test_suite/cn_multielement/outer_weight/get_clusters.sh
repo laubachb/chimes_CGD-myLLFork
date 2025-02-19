9f6cd9457533f9f97f13dc41da2728f9ca032c4c
Benjamin Robert Laubach <laubach2@ruby964.llnl.gov>
2025-02-19 08:30:38 -0800

    Added cluster composition to cluster distance list

--- test_suite/cn_multielement/outer_weight/get_clusters.sh
+++ test_suite/cn_multielement/outer_weight/get_clusters.sh
@@ -39,12 +39,14 @@ if [ 1 -eq 1 ] ; then
     
         time ./extract_clusters setup.in
         
-        for j in `ls 2b_clu-*.txt 3b_clu-*.txt 4b_clu-*.txt`
+        # Rename _clu-*.txt files with the tag
+        for j in 2b_clu-*.txt 3b_clu-*.txt 4b_clu-*.txt
         do
             mv "$j" "${tag}.${j}"
         done
 
-        for j in `ls clutypes_*b.txt`
+        # Process and rename clutypes files
+        for j in clutypes_*b.txt
         do
             if [[ $j == clutypes_2* ]]; then
                 sed -i '1d' "$j"  # Remove first line
@@ -56,7 +58,22 @@ if [ 1 -eq 1 ] ; then
             mv "$j" "${tag}.${j}"
         done
 
+        # Now paste the renamed clutypes files with the corresponding clu files
+        for j in ${tag}.2b_clu-*.txt ${tag}.3b_clu-*.txt ${tag}.4b_clu-*.txt
+        do
+            # Extract 2b, 3b, or 4b from filename
+            clu_type=$(echo "$j" | grep -o '[234]b')
 
+            # Correctly find the corresponding clutypes file
+            clutype_file="${tag}.clutypes_${clu_type}.txt"
+
+            if [[ -f "$clutype_file" ]]; then
+                paste "$j" "$clutype_file" > temp.txt && mv temp.txt "$j"
+            else
+                echo "Warning: Matching file $clutype_file not found for $j"
+            fi
+        done
+        
     done
 fi
 
